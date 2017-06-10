e3fa4d27ccc1e3715a98e75fef8eea086637c951
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-10 23:02:13 +0100

    Initialize project repository

---
+++ app/template/js/dist/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Just playing around, this doesn't really do anything
+(function (con) {
+  ['yes', 'no'].forEach(function (item) {
+    con.log(item);
+  });
+})(console);
---
+++ app/template/js/src/main.js
@@ -0,0 +1,6 @@
+// Just playing around, this doesn't really do anything
+((con) => {
+  ['yes', 'no'].forEach((item) => {
+    con.log(item);
+  })
+})(console);
