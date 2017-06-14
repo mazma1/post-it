f4f0373e3df0c71a1c21337a54d1445d1b67c91b
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-14 09:08:09 +0100

    Conditional statement to validate user input

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -6,13 +6,21 @@ const salt = bcrypt.genSaltSync(saltRounds);
 
 module.exports = {
   signup(req, res) {
-    const userData = {
-      email: req.body.email,
-      username: req.body.username,
-      password: bcrypt.hashSync(req.body.password, salt)
-    };
-    User.create(userData)
-      .then(user => res.status(201).send(user))
-      .catch(error => res.status(400).send(error));
-  },
+    if (req.body.email === '') {
+      res.json({
+        staus: false,
+        message: 'Email is required.'
+      });
+    }
+    else {
+      const userData = {
+        email: req.body.email,
+        username: req.body.username,
+        password: bcrypt.hashSync(req.body.password, salt)
+      };
+      User.create(userData)
+        .then(user => res.status(201).send(user))
+        .catch(error => res.status(400).send(error));
+    }
+  }
 };
