60e0b08c54449ebe4bb4fb3433d3a134847d9f2f
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-14 09:12:31 +0100

    Sign up route with no validation for input

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -6,21 +6,13 @@ const salt = bcrypt.genSaltSync(saltRounds);
 
 module.exports = {
   signup(req, res) {
-    if (req.body.email === '') {
-      res.json({
-        staus: false,
-        message: 'Email is required.'
-      });
-    }
-    else {
-      const userData = {
-        email: req.body.email,
-        username: req.body.username,
-        password: bcrypt.hashSync(req.body.password, salt)
-      };
-      User.create(userData)
-        .then(user => res.status(201).send(user))
-        .catch(error => res.status(400).send(error));
-    }
+    const userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: bcrypt.hashSync(req.body.password, salt)
+    };
+    User.create(userData)
+      .then(user => res.status(201).send(user))
+      .catch(error => res.status(400).send(error));
   }
 };
