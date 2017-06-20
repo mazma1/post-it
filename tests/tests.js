8d2bc9b27edf2ccd398c84b58574d1bff39631bd
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-20 14:16:54 +0100

    Add unit test for home page

--- tests/tests.js
+++ tests/tests.js
@@ -2,36 +2,26 @@
 const supertest = require('supertest');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const should = require('chai').should;
 const app = require('../app/server/app.js');
+
+const should = chai.should();
+chai.use(chaiHttp);
 
 
 const User = require('../app/server/models').User;
 const userController = require('../app/server/controllers/userController.js');
 const tokenAuth = require('../app/server/middlewares/tokenAuth.js');
 
-// Test for models
-describe('Post It Models', ()  => {  
-  
-  describe('Users', () => {
-    // This function will run before every test to clear database
-    beforeEach((done) => { //Before each test we empty the database
-      User.remove({}, (err) => 
+describe('Home page test', () => {
+  // #1 should return home page
+  it('should return status code 200', (done) => {
+    // calling home page api
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        res.status.should.equal(200);
+        res.error.should.equal(false);
         done();
       });
-    });
-  });
-
-  describe('/POST New User', () => {
-    it('it should POST a new user', (done) => {
-      chai.request(app)
-        .post('/api/user/signup')
-        .end((err, res) => {
-          res.should.have.status(201);
-          res.body.should.be.a('Object');
-          res.body.length.should.be.eql(0);
-          done();
-        });
-    });
   });
 });
