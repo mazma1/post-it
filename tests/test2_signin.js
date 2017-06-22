d48ff416092a7c8c94cd10038088658c804e5932
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-22 11:52:34 +0100

    Move production DB detils to .env and attempt to disable logs in command line during tests

--- tests/test2_signin.js
+++ tests/test2_signin.js
@@ -1,3 +1,4 @@
+process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app/server/app.js');
---
+++ tests/test3_creategroup.js
@@ -0,0 +1,63 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app/server/app.js');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../app/server/models').User;
+const Group = require('../app/server/models').Group;
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+const saltRounds = 7;
+const salt = bcrypt.genSaltSync(saltRounds);
+
+describe('POST /api/group', () => {
+  let token;
+  let userId;
+
+  beforeEach((done) => {
+    User.destroy({ where: {} })
+      .then(() => {
+        User.create({
+          id: 1,
+          email: 'mary@gmail.com',
+          username: 'mary',
+          password: bcrypt.hashSync('1234', salt)
+        })
+        .then((user) => {
+          userId = user[0];
+          token = jwt.sign({ user_id: userId }, 'qWgdh*ujh7778Qwee');
+          done();
+        })
+        .catch((error) => {
+          done(error);
+        });
+      });
+  });
+
+
+  describe('status 201', () => {
+    it('returns successfully create group', (done) => {
+      const group = {
+        id: 1,
+        group_name: 'Test Group 1',
+        user_id: userId
+      };
+      chai.request(app)
+        .post('/api/group')
+          .set('x-access-token', token)
+          .send(group)
+          .end((err, res) => {
+            res.status.should.equal(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('message').eql('Group was successfully created');
+            res.body.should.have.property('groupName').eql('Test Group 1');
+            res.body.should.have.property('groupOwner').eql(1);
+            done(err);
+          });
+    });
+  });
+});
+
+
