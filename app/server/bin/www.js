fa31e753dd8b349817119921503cc0c5bc31f303
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-24 08:52:08 +0100

    Include IP address so as to access my localhost from my mobile phone

--- app/server/bin/www.js
+++ app/server/bin/www.js
@@ -1,6 +1,8 @@
 // This will be our application entry. We'll setup our server here.
 const app = require('../index'); // The express app we just created on index.js
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, '192.168.43.196' || 'localhost', () => {
+  console.log('Server started on port ' + port);
 });
