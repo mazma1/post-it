6d0fb7f8bab5ac685ad58560256ac6263d1448ce
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-28 13:52:27 +0100

    Removed ExtractText Plugin to order my css files corectly. Couldn't get it to extract bootstrap.css and materialize.css

--- app/client/index.js
+++ app/client/index.js
@@ -2,24 +2,12 @@ import AppRoutes from './components/AppRoutes';
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+
 require('bootstrap-loader');
-
-require('materialize-css/dist/css/materialize.min.css');
-require('materialize-css/dist/js/materialize.min.js');
-require('materialize-css/dist/fonts/roboto/Roboto-Bold.woff');
-require('materialize-css/dist/fonts/roboto/Roboto-Bold.woff2');
-require('materialize-css/dist/fonts/roboto/Roboto-Light.woff');
-require('materialize-css/dist/fonts/roboto/Roboto-Light.woff2');
-require('materialize-css/dist/fonts/roboto/Roboto-Medium.woff');
-require('materialize-css/dist/fonts/roboto/Roboto-Medium.woff2');
-require('materialize-css/dist/fonts/roboto/Roboto-Regular.woff');
-require('materialize-css/dist/fonts/roboto/Roboto-Regular.woff2');
-require('materialize-css/dist/fonts/roboto/Roboto-Thin.woff');
-require('materialize-css/dist/fonts/roboto/Roboto-Thin.woff2');
-
-// require('bootstrap-loader');
-
+require('materialize-loader');
 require('./static/scss/style.scss');
+
+
 
 const Client = require('./components/Client');
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
+    // '../post-it/node_modules/materialize-loader/materialize.config.js',
     '../post-it/app/client/index.js'
   ],
   output: {
@@ -15,20 +16,22 @@ module.exports = {
     rules: [
       { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'url-loader?importLoaders=1&limit=100000' },
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
       { test: /bootstrap\/dist\/js\/umd\//, loader: 'imports?jQuery=jquery' },
-      // { test: /\.(css)$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }), exclude: /node_modules/ },
-      { test: /\.(scss)$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }), exclude: /node_modules/ }
+      // { test: /\.css$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: ['css-loader'] }), exclude: /node_modules/ },
+      // { test: /\.(scss)$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!sass-loader' }), exclude: /node_modules/ }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/client/index.html'
     }),
-    new ExtractTextPlugin({
-      filename: 'css/style.css',
-      allChunks: true
-    }),
+    // new ExtractTextPlugin({
+    //   filename: 'css/style.css',
+    //   allChunks: true
+    // }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery'
