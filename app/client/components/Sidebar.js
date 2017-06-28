9647f3935dc94aeeb172d977f35089c0b8715c63
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-28 18:39:27 +0100

    Added the sidebar component but media query does not work

--- app/client/components/Sidebar.js
+++ app/client/components/Sidebar.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
-const Brand = () => ({
-  render() {
-    return (
-      <div id="brand-name">
-        Post It
-      </div>
-    );
-  }
-});
+const Brand = () => {
+  return (
+    <div id="brand-name">
+      Post It
+    </div>
+  );
+};
+
+const MobileToggleBtn = () => {
+  return (
+    <button type="button" className="mobile-nav-btn  navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-content" aria-expanded="false" aria-controls="menu-content">
+      <span className="sr-only">Toggle navigation</span>
+      <span className="icon-bar"></span>
+      <span className="icon-bar"></span>
+      <span className="icon-bar"></span>
+    </button>
+  );
+};
 
 class Sidebar extends React.Component {
   render() {
@@ -16,8 +26,35 @@ class Sidebar extends React.Component {
       <aside className="navbar-default mobile-navbar">
         <div id="sidebar">
           <Brand />
-          <h1>I am the Sidebar</h1>
-          <h3>Testing Autoscrolling</h3>
+          <MobileToggleBtn />
+
+          <ul className="nav nav-pills nav-stacked navbar-fixed-side navbar-collapse collapse" id="menu-content">
+            <li role="presentation" className="active">
+              <NavLink activeClassName="active" to="#" >
+                Group 1
+              </NavLink>
+            </li>
+            <li role="presentation">
+              <NavLink activeClassName="active" to="#" >
+                Group 2
+              </NavLink>
+            </li>
+            <li role="presentation">
+              <NavLink activeClassName="active" to="#" >
+                Group 3
+              </NavLink>
+            </li>
+            <li role="presentation">
+              <NavLink activeClassName="active" to="#" >
+                Group 4
+              </NavLink>
+            </li>
+            <li role="presentation">
+              <NavLink activeClassName="active" to="#" >
+                Group 5
+              </NavLink>
+            </li>
+          </ul>
         </div>
       </aside>
     );
