55477589676c220791e38430af7fc8f7f1f0e6ad
Mary Mazi <mazi.mary.o@gmail.com>
2017-07-01 08:22:40 +0100

    Add signup form component

--- app/client/components/auth/SigninForm.js
+++ app/client/components/auth/SigninForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-class SignUp extends React.Component {
+class SignIn extends React.Component {
   render() {
     return (
       <div className="background">
-        
+     
       </div>
     );
   }
 }
 
-module.exports = SignUp;
+module.exports = SignIn;
--- app/client/index.js
+++ app/client/index.js
@@ -4,6 +4,7 @@ const ReactDOM = require('react-dom');
 require('bootstrap-loader');
 require('materialize-loader');
 require('./static/scss/style.scss');
+require('../../node_modules/jquery/dist/jquery.min');
 
 
 const Client = require('./components/Client');
