ec830a3804240786d3db8566632277ea6388b587
Mary Mazi <mazi.mary.o@gmail.com>
2017-07-03 08:13:37 +0100

    Add state to signup form

--- app/client/components/sign-up/SignupForm.js
+++ app/client/components/sign-up/SignupForm.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
 class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstname: '',
+      lastname: '',
+      username: '',
+      email: '',
+      password: '',
+      confirm_password: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSignupClick = this.onSignupClick.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSignupClick(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <form className="col s12 auth-form">
         <div className="row">
           <div className="input-field auth-field col m6 s12">
             <i className="material-icons prefix">perm_identity</i>
-            <input id="icon_prefix" type="text" className="validate" name='firstname'/>
+            <input id="icon_prefix" type="text" className="validate" name='firstname' value={this.state.firstname} onChange={this.onChange}/>
             <label htmlFor="icon_prefix">First Name</label>
           </div>
 
           <div className="input-field auth-field col m6 s12">
             <i className="material-icons prefix">perm_identity</i>
-            <input id="icon_prefix" type="text" className="validate" name='lastname'/>
+            <input id="icon_prefix" type="text" className="validate" name='lastname' value={this.state.lastname} onChange={this.onChange}/>
             <label htmlFor="icon_prefix">Last Name</label>
           </div>
         </div>
@@ -21,7 +45,7 @@ class SignupForm extends React.Component {
         <div className="row">
           <div className="input-field auth-field col s12">
             <i className="material-icons prefix">perm_identity</i>
-            <input id="icon_prefix" type="text" className="validate" name='username'/>
+            <input id="icon_prefix" type="text" className="validate" name='username' value={this.state.username} onChange={this.onChange}/>
             <label htmlFor="icon_prefix">Username</label>
           </div>
         </div>
@@ -29,7 +53,7 @@ class SignupForm extends React.Component {
         <div className="row">
           <div className="input-field auth-field col s12">
             <i className="material-icons prefix">email</i>
-            <input id="icon_prefix" type="email" className="validate" name='email'/>
+            <input id="icon_prefix" type="email" className="validate" name='email' value={this.state.email} onChange={this.onChange}/>
             <label htmlFor="icon_prefix">Email</label>
           </div>
         </div>
@@ -37,7 +61,7 @@ class SignupForm extends React.Component {
         <div className="row">
           <div className="input-field auth-field col s12">
             <i className="material-icons prefix">https</i>
-            <input id="icon_prefix" type="password" className="validate" name='password'/>
+            <input id="icon_prefix" type="password" className="validate" name='password' value={this.state.password} onChange={this.onChange}/>
             <label htmlFor="icon_prefix">Password</label>
           </div>
         </div>
@@ -45,14 +69,14 @@ class SignupForm extends React.Component {
         <div className="row">
           <div className="input-field auth-field col s12">
             <i className="material-icons prefix">https</i>
-            <input id="icon_prefix" type="password" className="validate" name='confirm_password'/>
+            <input id="icon_prefix" type="password" className="validate" name='confirm_password' value={this.state.confirm_password} onChange={this.onChange}/>
             <label htmlFor="icon_prefix">Confirm Password</label>
           </div>
         </div>
           
         <div className="row">
           <div className="input-field col s12">
-            <a href="#" className="auth-btn btn waves-effect waves-light col s12">Sign Up</a>
+            <a className="auth-btn btn waves-effect waves-light col s12" onClick={this.onSignupClick}>Sign Up</a>
           </div>
         </div>
 
@@ -67,3 +91,4 @@ class SignupForm extends React.Component {
 }
 
 module.exports = SignupForm;
+
