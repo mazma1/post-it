f7978a61b4db72b915159d7d1d056d38a59ea25e
Mary Mazi <mazi.mary.o@gmail.com>
2017-07-05 18:39:52 +0100

    Refactor signup server side validation for easy error reporting on the client

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -3,6 +3,7 @@ const User = require('../models').User;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
+const isEmpty = require('lodash/isEmpty');
 
 const saltRounds = 7;
 const salt = bcrypt.genSaltSync(saltRounds);
@@ -11,15 +12,55 @@ module.exports = {
 
   // Method to signup a user
   signup: (req, res) => {
-    if (!req.body.email) {
-      res.status(400).send({ success: false, message: 'Email is required' });
-    } else if (!validator.isEmail(req.body.email)) {
-      res.status(400).send({ success: false, message: 'Incorrect email syntax' });
-    } else if (!req.body.username) {
-      res.status(400).send({ success: false, message: 'Username is required' });
-    } else if (!req.body.password) {
-      res.status(400).send({ success: false, message: 'Password is required' });
-    } else if (req.body.username && req.body.email) {
+    // if (!req.body.email) {
+    //   res.status(400).send({ success: false, message: 'Email is required' });
+    // } else if (!validator.isEmail(req.body.email)) {
+    //   res.status(400).send({ success: false, message: 'Incorrect email syntax' });
+    // } else if (!req.body.username) {
+    //   res.status(400).send({ success: false, message: 'Username is required' });
+    // } else if (!req.body.password) {
+    //   res.status(400).send({ success: false, message: 'Password is required' });
+    // } else if (req.body.username && req.body.email) {
+
+
+    function validateInput(data) {
+      let errors = {};
+      
+      if (!data.firstname) {
+        errors.firstname = 'This field is required';
+      }
+      if (!data.lastname) {
+        errors.lastname = 'This field is required';
+      }
+      if (!data.email) {
+        errors.email = 'This field is required';
+      } else if (!validator.isEmail(data.email)) {
+        errors.email = 'Invalid email';
+      }
+      if (!data.username) {
+        errors.username = 'This field is required';
+      }
+      if (!data.password) {
+        errors.password = 'This field is required';
+      }
+      if (!data.confirm_password) {
+        errors.confirm_password = 'This field is required';
+      } else if (!validator.equals(data.password, data.confirm_password)) {
+        errors.confirm_password = 'Passwords must match';
+      }
+      return {
+        errors,
+        valid: isEmpty(errors)
+      };
+    }
+
+    // res.send(typeof (req.body.username));
+
+    const { errors, valid } = validateInput(req.body);
+
+    if (!valid) {
+      res.status(400).send(errors);
+    } else {
       User.findOne({
         where: {
           username: req.body.username
