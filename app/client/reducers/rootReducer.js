c72e4479f871b4f61fc4b8a36daa1b778a1fcfeb
Mary Mazi <mazi.mary.o@gmail.com>
2017-07-11 12:21:11 +0100

    Add flash message reducer - for successful sign in

--- app/client/reducers/rootReducer.js
+++ app/client/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import FlashMessageReducer from '../reducers/reducer_flashMessage';
 import AuthenticatedUserReducer from '../reducers/reducer_authenticatedUser';
 
 // Mapping of our state
 const rootReducer = combineReducers({
+  flashMessages: FlashMessageReducer,
   signedInUser: AuthenticatedUserReducer
 });
 
