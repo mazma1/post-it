409140296c39a99a5a63389aed9ce3976a8655d8
Mary Mazi <mazi.mary.o@gmail.com>
2017-07-25 09:17:03 +0100

    Add initial state to groupMessages reducer to determine status of request to load group messages and loads appropriate components to client

--- app/client/reducers/reducer_groupMessages.js
+++ app/client/reducers/reducer_groupMessages.js
@@ -1,15 +1,23 @@
 import { SET_GROUP_MESSAGES, POST_NEW_MESSAGE } from '../actions/types';
 
-export default (state = [], action = {}) => {
+const initialState = {
+  isLoading: true,
+  messages: []
+};
+
+export default (state = initialState, action = {}) => {
   switch (action.type) {
     case SET_GROUP_MESSAGES:
-      return action.messages;
+      return {
+        isLoading: false,
+        messages: action.messages
+      };
 
-    case POST_NEW_MESSAGE:
-      return [
-        ...state,
-        action.message
-      ];
+    // case POST_NEW_MESSAGE:
+    //   return [
+    //     ...state,
+    //     action.message
+    //   ];
 
     default:
       return state;
