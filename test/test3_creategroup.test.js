7149ff51e336a354c958618fc4b2992906a9b8b3
Mary Mazi <mazi.mary.o@gmail.com>
2017-07-27 15:49:15 +0100

    Successfully generates token for testing protected routes. All tests pass but test suite does not exit

--- test/test3_creategroup.test.js
+++ test/test3_creategroup.test.js
@@ -2,7 +2,6 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-// const sinon = require('sinon');
 const app = require('../app/server/app.js');
 const User = require('../app/server/models').User;
 const Group = require('../app/server/models').Group;
@@ -15,52 +14,46 @@ const salt = bcrypt.genSaltSync(saltRounds);
 
 describe('POST /api/group', () => {
   let token;
-  // let userId;
-  // let tokenAuthSpy;
-  let testGroup;
-
 
   before((done) => {
-    User.destroy({ where: {} })
-      .then(() => {
-        User.create({
-          firstname: 'Mary',
-          lastname: 'Mazi',
-          username: 'mary',
-          email: 'mary@gmail.com',
-          password: bcrypt.hashSync('1234', salt),
-          confirm_password: bcrypt.hashSync('1234', salt)
-        })
-        .then(user => {
-          Group.destroy({ where: {} })
-            .then(() => Group.bulkCreate([
-              {
-                group_name: 'Test Group 1',
-                user_id: user.id
-              },
-              {
-                group_name: 'Test Group 2',
-                user_id: user.id
-              }
-            ]))
-            .then((groups) => {
-              testGroup = groups[0];
-              token = jwt.sign({ data: user }, process.env.TOKEN_SECRET);
-              done();
-            });
-        })
-        .catch((error) => {
-          done(error);
-        });
+    User.destroy({
+      cascade: true,
+      truncate: true,
+      restartIdentity: true
+    })
+    .then(() => {
+      User.create({
+        firstname: 'Mary',
+        lastname: 'Mazi',
+        username: 'mary',
+        email: 'mary@gmail.com',
+        password: bcrypt.hashSync('1234', salt),
+        confirm_password: bcrypt.hashSync('1234', salt)
+      })
+      .then((user) => {
+        Group.bulkCreate([
+          {
+            group_name: 'Test Group 1',
+            user_id: user.id
+          },
+          {
+            group_name: 'Test Group 2',
+            user_id: user.id
+          }
+        ]);
+        token = jwt.sign({ data: user }, process.env.TOKEN_SECRET);
       });
+      done();
+    })
+    .catch((error) => {
+      done(error);
+    });
   });
 
   describe('POST /api/group Route', () => {
     it('returns status 403 when user is not logged in (no token is provided)', (done) => {
       const group = {
-        // id: 1,
         group_name: 'Test Group 4'
-        // user_id: userId
       };
       chai.request(app).post('/api/group')
         .send(group)
@@ -73,27 +66,24 @@ describe('POST /api/group', () => {
     });
   });
 
-
-  // describe('status 201', () => {
-  //   it('returns successfully create group', (done) => {
-  //     const group = {
-  //       // id: 1,
-  //       group_name: 'Test Group 3'
-  //       // user_id: userId
-  //     };
-  //     chai.request(app).post('/api/group')
-  //       .set('x-access-token', `${token}`)
-  //       .send(group)
-  //       .end((err, res) => {
-  //         res.status.should.equal(200);
-  //         res.body.should.be.a('object');
-  //         res.body.should.have.property('message').eql('Group was successfully created');
-  //         res.body.should.have.property('groupName').eql('Test Group 3');
-  //         res.body.should.have.property('groupOwner').eql(1);
-  //         done();
-  //       });
-  //   });
-  // });
+  describe('status 201', () => {
+    it('returns successfully create group', (done) => {
+      const group = {
+        group_name: 'Test Group 3'
+      };
+      chai.request(app).post('/api/group')
+        .set('x-access-token', token)
+        .send(group)
+        .end((err, res) => {
+          res.status.should.equal(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Group was successfully created');
+          res.body.should.have.property('groupName').eql('Test Group 3');
+          res.body.should.have.property('groupOwner').eql(1);
+          done();
+        });
+    });
+  });
 });
 
 
