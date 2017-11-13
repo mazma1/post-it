287048bcd4108eeda4b8cf6d37069358ecc313f7
Mary Mazi <mazi.mary.o@gmail.com>
2017-11-13 09:54:56 +0100

    - Disable Request Reset button when request to submit email is still in progress

--- app/server/controllers/user.js
+++ app/server/controllers/user.js
@@ -258,7 +258,7 @@ export default {
         })
         .catch(error => res.status(500).send(error.message));
       } else {
-        res.status(404).send({ message: 'User does not exist' });
+        res.status(404).send({ email: 'User does not exist' });
       }
     })
     .catch(error => res.status(500).send(error.message));
